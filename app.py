31ff98f8d3375342a038061a1ee10b6e928929d5
Harisudhan5 <speaktoharisudhan@gmail.com>
2024-02-24 16:57:44 +0530

    app completed

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-import numpy as np
-import tensorflow as tf
 import streamlit as st
-from tensorflow.keras.models import load_model
 import cv2
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+import tensorflow as tf
 
+def extract_text(image):
+    loaded_model = load_model('Model//best_model.h5')
+    resized_image = cv2.resize(loaded_model,(256,256,3))
+    result = loaded_model.predict(np.expand_dims(resized_image/255, 0))
+    if result > 0.5:
+        return "Non Parasitized"
+    else:
+        return "Parasitized"
 
-st.title("Malarial Cell Prediction Through Microscopic Image")
-uploaded_file = st.file_uploader("Upload an Image of RBC Cell...", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-    if st.button("Predict"):
-        img = cv2.imread(uploaded_file)
-        resized_img = cv2.resize(img,(256,256,3))
-        loaded_model = load_model('Model//best_model.h5')
-        result = loaded_model.predict(np.expand_dims(resized_img/255, 0))
-        if result > 0.5:
-            st.text("The cell is Non Parasitized")
-        else:
-            st.text("The cell is Parasitized")
-        
+def main():
+    
+    st.title("Malarial Cell Prediction Using Microscopic Image")
 
+    uploaded_file = st.file_uploader("Upload an image - File format supported - [jpeg, png, jpg]", type=["jpg", "jpeg", "png"])
 
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        if st.button("Mine the Image"):
+            # Convert PIL Image to OpenCV format
+            image_cv = np.array(image)
+
+            # Output 1: Extracted Text
+            text_result = extract_text(image_cv)
+            st.subheader("Result")
+            st.text(text_result)
+
+if __name__ == "__main__":
+    main()
